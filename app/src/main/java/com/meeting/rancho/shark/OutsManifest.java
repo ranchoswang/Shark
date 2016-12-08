b3e397ab44c97f649e0dd0dbfa2db9d6d0dbcb25
Rancho Wang <ranchoswang@gmail.com>
2016-12-08 21:23:09 +0800

    new activity

---
+++ app/src/main/java/com/meeting/rancho/shark/OutsManifest.java
@@ -0,0 +1,15 @@
+package com.meeting.rancho.shark;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+
+public class OutsManifest extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_outs_manifest);
+    }
+
+}
